142bad289079243fdea0d142af3a60b2bb53c2a2
Sparkles2000 <rucker.anna2000@gmail.com>
2021-05-19 22:12:55 -0500

    added code to About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { image } from "../data/data";
 
+const aboutContainer = <About />
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+    <p>I love Designing!</p>
+    <img src={image} alt="I made this"/>
+  </div>
+  )
 }
 
 export default About;
